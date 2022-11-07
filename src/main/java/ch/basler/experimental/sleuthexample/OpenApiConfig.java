6e98f32bb20a3c078b076294af9329b8d00dc4f6
Dane Leube <dane.leube@baloise.ch>
2022-11-07 11:14:29 +0100

    feat: Add new baggage header for cid

--- src/main/java/ch/basler/experimental/sleuthexample/OpenApiConfig.java
+++ src/main/java/ch/basler/experimental/sleuthexample/OpenApiConfig.java
@@ -1,14 +1,16 @@
 package ch.basler.experimental.sleuthexample;
 
-import brave.propagation.B3Propagation;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
+
+import brave.propagation.B3Propagation;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 
 @Configuration
 public class OpenApiConfig {
@@ -21,6 +23,17 @@ public class OpenApiConfig {
             .name(header).description("Specification at https://github.com/openzipkin/b3-propagation");
         operation.addParametersItem(parameter);
       });
+
+      final Schema<String> baggageSchema = new Schema<>();
+      baggageSchema.type("string");
+      final Parameter baggage = new Parameter()
+              .in(ParameterIn.HEADER.toString())
+              .name("baggage")
+              .description("Additional tracing context. See <a href=\"https://www.w3.org/TR/baggage/\">baggage</a> for more information. ")
+              .example("cid=test12345")
+              .required(false)
+              .schema(baggageSchema);
+      operation.addParametersItem(baggage);
       return operation;
     };
   }
